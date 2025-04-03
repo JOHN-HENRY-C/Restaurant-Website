76138f215ae217a2202048152e6abc401499b0d5
JOHN-HENRY-C <johnhenryacharles@gmail.com>
2025-04-03 21:08:28 +0530

    Responsive Change

--- los_angleles.js
+++ los_angleles.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     let images = document.querySelectorAll(".Info_1");
 
-    let observer = new IntersectionObserver((entries) => {
+    let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("active");
-            } else {
+                observer.unobserve(entry.target); 
+            }else {
                 entry.target.classList.remove("active"); 
             }
         });
-    }, { threshold: 0.1 }); 
+    }, { threshold: 0.1 });
 
     images.forEach((img) => {
         observer.observe(img);
     });
 });
-
 
